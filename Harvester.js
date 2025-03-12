2e727c8589ba9e041706620c9093c613c53d9135
jaskinner <jon.a.skinner1@gmail.com>
2025-03-12 15:55:08 -0400

    separated some truck logic

--- Harvester.js
+++ Harvester.js
@@ -16,33 +16,44 @@ class Harvester extends Creep {
         return this.pos().findClosestByRange(FIND_SOURCES_ACTIVE);
     }
 
+    getType() {
+        return this.getMemory().type;
+    }
+
+    harvest() {
+        var source = this.getClosestEnergySource();
+
+        if (source) {
+            this.harvestFrom(source);
+        }
+    }
+
     structureFilter(structure) {
         return this.energyStores().includes(structure.structureType) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+    }
+
+    transfer() {
+        if (this.getType() === 'shovel') {
+            return;
+        }
+        var targets = this.getRoom().find(FIND_STRUCTURES, {
+            filter: this.structureFilter.bind(this)
+        });
+
+        if (targets.length > 0) {
+            this.transferTo(targets[0], RESOURCE_ENERGY);
+        } else {
+            this.getCreep().drop(RESOURCE_ENERGY);
+        }
     }
 
     run() {
         this.decideState();
 
         if (this.getMemory().state === 'harvesting') {
-            var droppedSource = this.pos().findClosestByRange(FIND_DROPPED_RESOURCES) || this.pos().findClosestByRange(FIND_TOMBSTONES);
-            var source = this.getClosestEnergySource();
-
-            if (droppedSource) {
-                this.pickup(droppedSource);
-            } else {
-                this.harvestFrom(source);
-            }
-            this.harvestFrom(source);
+            this.harvest()
         } else if (this.getMemory().state === 'transfering') {
-            var targets = this.getRoom().find(FIND_STRUCTURES, {
-                filter: this.structureFilter.bind(this)
-            });
-
-            if (targets.length > 0) {
-                this.transferTo(targets[0], RESOURCE_ENERGY);
-            } else {
-                this.getCreep().drop(RESOURCE_ENERGY);
-            }
+            this.transfer();
         } else if (this.getMemory().state === 'recycling') {
             if (Game.spawns['Spawn1'].recycleCreep(this.getCreep()) === ERR_NOT_IN_RANGE) {
                 this.moveTo(Game.spawns['Spawn1']);
@@ -59,10 +70,13 @@ class Truck extends Harvester {
         }
     }
 
-    run() {
-        // stub
-    }
+    harvest() {
+        var source = this.pos().findClosestByRange(FIND_DROPPED_RESOURCES) || this.pos().findClosestByRange(FIND_TOMBSTONES);
 
+        if (source) {
+            this.pickup(source);
+        }
+    }
 }
 
 module.exports = {
---
+++ Repairer.js
@@ -0,0 +1,47 @@
+var Creep = require('Creep');
+
+module.exports = class Upgrader extends Creep {
+    constructor(creep) {
+        super(creep);
+        if (!this.getMemory().state) {
+            this.getMemory().state = 'harvesting';
+        }
+    }
+
+    upgradeController(controller) {
+        if (this.getCreep().upgradeController(controller) == ERR_NOT_IN_RANGE) {
+            this.moveTo(controller);
+        }
+    }
+
+    decideState() {
+        if (this.getMemory().state === 'harvesting' && this.creep.store.getFreeCapacity() === 0) {
+            this.getMemory().state = 'upgrading';
+        } else if (this.getMemory().state === 'upgrading' && this.creep.store.getUsedCapacity() === 0) {
+            this.getMemory().state = 'harvesting';
+        }
+    }
+
+    run() {
+        this.decideState();
+
+        if (this.getMemory().state === 'harvesting') {
+            var container = this.pos().findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType === STRUCTURE_CONTAINER && structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            
+            if (container) {
+                this.transferFrom(container, RESOURCE_ENERGY);
+            }
+        } else if (this.getMemory().state === 'upgrading') {
+            var controller = this.getRoom().controller;
+            this.upgradeController(controller);
+        } else if (this.getMemory().state === 'recycling') {
+            if (Game.spawns['Spawn1'].recycleCreep(this.getCreep()) === ERR_NOT_IN_RANGE) {
+                this.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+    }
+}
